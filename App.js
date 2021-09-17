a061de9d0620fc73243bcccc97ef3eeb07d07190
XJ <ianyian@gmail.com>
2021-09-17 21:56:32 +0800

    update

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { useDimensions } from "@react-native-community/hooks";
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  Alert,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableHighlight,
+  Platform,
+  Dimensions,
+  ViewPagerAndroidComponent,
+} from "react-native";
+
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <ViewImageScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
